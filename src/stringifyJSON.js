15a36b4f9ae42c5890c3469c2416d8df38e4b4fd
Nicholas <nicholas@niceguys.com>
2014-02-17 20:51:09 -0800

    Temp commit to get specrunner working

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,29 @@
 
 // but you don't so you're going to have to write it from scratch:
 var stringifyJSON = function (obj) {
-  // your code goes here
+  switch(typeof(obj)){
+    case 'function':
+    case 'undefined': //Multicriteria case
+      return 'null';
+    case 'number':
+    case 'boolean':
+      return '' + obj; //Force it into a string
+    case 'string':
+      return '"' + obj + '"';
+
+    case 'object':
+      obj == null ? return 'null' :
+
+        var returnarray = _(obj).map(function(item, position, obj) {
+          if(typeof(item) !== 'object'){ 
+            if(Array.isArray(obj)){
+              return stringifyJSON(item);
+            } else {
+              return position + ':' + item;
+            }
+          } else {
+            return stringifyJSON(item);
+          }
+        });
+      return true;  
 };
