791955309c3453a25b5336b22d7afb2388b9fcc7
Nicholas <nicholas@niceguys.com>
2014-02-18 16:57:15 -0800

    Finished gebcn with a mixed functional imperative style. Minus pts for local global.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,4 +6,26 @@
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
   // your code here
+  var allElements = document.body;
+  var matchingElements = [];
+
+  var isClass = function(element, className) {
+    return _(element.classList).any(function(item) {
+      return item == className;
+    });
+  }
+
+  var pullMatching = function(domElement) { 
+    if(domElement.childElementCount){
+      _(domElement.children).each(function(element) {
+        pullMatching(element);
+      });
+    } 
+    if(isClass(domElement, className)) {
+      matchingElements.push(domElement);
+    }
+  } 
+
+  pullMatching(allElements);
+  return matchingElements;
 };
